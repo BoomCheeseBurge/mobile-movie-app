1ff4614ae8705924677b58961b0241f54c01ec62
Brandon John <80831628+BoomCheeseBurge@users.noreply.github.com>
2025-07-23 21:30:46 +0700

    Setup the TMDB API

---
+++ services/api.ts
@@ -0,0 +1,38 @@
+// TMDB Configuration
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        // Inform the acceptable data
+        accept: 'application/json',
+        // Auth header
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+
+export const fetchMovies = async ({ query } : { query: string }) => {
+
+    // Targeted API endpoint
+    // Consider the query search for finding movies if any
+    const endpoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : 
+        `${TMDB_CONFIG.BASE_URL}/dicover/movie?sort_by=popularity.desc`;
+
+    // Get the response
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    // Check if the response is empty
+    if (!response.ok) {
+        throw new Error('Failed to fetch movies', response.statusText);
+    } 
+
+    // Get the transformed data
+    const data = await response.json();
+
+    // Return the movie results
+    return data.results;
+}
